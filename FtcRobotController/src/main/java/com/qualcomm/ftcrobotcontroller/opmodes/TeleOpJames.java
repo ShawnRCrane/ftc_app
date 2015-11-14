19fe1dc9fa1d4accb20343c24878bd2e4cbdd810
RedCater <RedCater360@yahoo.com>
2015-11-14 15:29:23 -0600

    Update
    Updated SDK

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpJames.java
@@ -0,0 +1,34 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+public class TeleOpJames extends OpMode{
+
+    DcMotor motorRight;
+    DcMotor motorLeft;
+
+    @Override
+    public void init() {
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        float leftStick = gamepad1.left_stick_y;
+        float rightStick = gamepad1.right_stick_y;
+
+        if (rightStick < 0.10 && rightStick > -0.10)
+            rightStick = 0;
+        if (leftStick < 0.10 && leftStick > -0.10)
+            leftStick = 0;
+
+        motorRight.setPower(rightStick);
+        motorLeft.setPower(leftStick);
+    }
+
+}
